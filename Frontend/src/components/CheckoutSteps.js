13493f20ace2844468a1abe0bcc770c22b78695e
MarioLivimoro <jcvdbocanegra@gmail.com>
2024-06-01 18:55:57 -0500

    Renovacion del Proyecto

--- Frontend/src/components/CheckoutSteps.js
+++ Frontend/src/components/CheckoutSteps.js
@@ -9,25 +9,3 @@ export const CheckoutSteps = {
 `;
   },
 };
-export const CheckoutSteps = {
-  render: (props) => {
-    return `
-<div class="checkout-steps">
-<div class="${props.step1 ? "active" : ""}">Signin</div>
-<div class="${props.step2 ? "active" : ""}">Shipping</div>
-<div class="${props.step3 ? "active" : ""}">Payment</div>
-<div class="${props.step4 ? "active" : ""}">Place Order</div>
-`;
-  },
-};
-export const CheckoutSteps = {
-  render: (props) => {
-    return `
-<div class="checkout-steps">
-<div class="${props.step1 ? "active" : ""}">Signin</div>
-<div class="${props.step2 ? "active" : ""}">Shipping</div>
-<div class="${props.step3 ? "active" : ""}">Payment</div>
-<div class="${props.step4 ? "active" : ""}">Place Order</div>
-`;
-  },
-};
